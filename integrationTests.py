923bc1ab7022a742dc4d8e6cf7867100a673e2c3
Ash Yveth Cudiamat <acudiamat@nevada.unr.edu>
2020-02-11 17:34:57 -0800

    2 integration tests

---
+++ integrationTests.py
@@ -0,0 +1,25 @@
+from tictactoe import Board
+from board_controller import Board_controller
+from modelView import ModelView
+from playToken import PlayToken
+
+import unittest
+
+class IntegrationTests(unittest.TestCase):
+
+    # Test if mark square in controller changes board
+    def test_markControllerMarkBoard(self):
+        b = Board()
+        c = Board_controller(b)
+        self.assertTrue(c.mark_square(0,0, PlayToken.O))
+
+    # Test if win from board registers to controller
+    def test_winSuccessfullyPassedFromBoardModel(self):
+        b = Board()
+        c = Board_controller(b)
+        c.mark_square(0,0, PlayToken.O)
+        c.mark_square(1,0, PlayToken.O)
+        c.mark_square(2,0, PlayToken.O)
+        self.assertTrue(c.check_win())
+        
+    
